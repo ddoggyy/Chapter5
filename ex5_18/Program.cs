8e54f7e1cab52a106acab7913b3a12f1e97b4fff
USER <USER@DESKTOP-CPMLSKC>
2025-04-04 11:30:50 +0900

    초기 커밋.

---
+++ ex5_18/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex5_18
+{
+    internal class Program
+    {   
+        class Student
+        {
+            public string id;
+            public string name;
+            public int grade;
+            public string major;
+            public DateTime birthday;
+        }
+
+        static void Main(string[] args)
+        {    
+        }
+    }
+}
---
+++ ex5_6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex5_6
+{
+    internal class Program
+    {
+        class Product
+        {
+            public string name = "default";
+            public int price = 1000;
+            public static double PI = 3.14;
+        }
+
+        static void Main(string[] args)
+        {
+            Product prod = new Product() { name = "감자", price = 5000 };
+
+            Console.WriteLine($"{prod.name} : {prod.price}원");
+            Console.WriteLine(Math.PI);
+            Console.WriteLine(Product.PI);
+        }
+    }
+}
